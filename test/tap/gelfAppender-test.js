5183aaf9ec12a7b72845eafe6b2f76bfdd07bde3
Gareth Jones <gareth.jones@shinetech.com>
2018-02-20 08:27:31 +1100

    chore(coverage): full test coverage

--- test/tap/gelfAppender-test.js
+++ test/tap/gelfAppender-test.js
@@ -16,13 +16,14 @@ const setupLogging = function (options, category, compressedLength) {
         this.closed = true;
         if (cb) cb();
       },
-      send: function (pkt, offset, pktLength, port, host) {
+      send: function (pkt, offset, pktLength, port, host, cb) {
         fakeDgram.sent = true;
         this.packet = pkt;
         this.offset = offset;
         this.packetLength = pktLength;
         this.port = port;
         this.host = host;
+        this.cb = cb;
       }
     },
     createSocket: function (type) {
@@ -114,6 +115,11 @@ test('log4js gelfAppender', (batch) => {
     setup.logger.info('This is a test');
 
     const dgram = setup.dgram;
+    setup.dgram.socket.cb();
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
 
     t.test('dgram packet should be sent via udp to the localhost gelf server', (assert) => {
       assert.equal(dgram.type, 'udp4');
@@ -136,9 +142,29 @@ test('log4js gelfAppender', (batch) => {
     t.end();
   });
 
+  batch.test('when dgram send returns an error', (t) => {
+    const setup = setupLogging();
+    setup.logger.info('This is also a test');
+    setup.dgram.socket.cb(new Error('oh no'));
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
+
+    t.test('should be logged to console.error', (assert) => {
+      assert.match(setup.console.message, /oh no/);
+      assert.end();
+    });
+    t.end();
+  });
+
   batch.test('with a message longer than 8k', (t) => {
     const setup = setupLogging(undefined, undefined, 10240);
     setup.logger.info('Blah.');
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
 
     t.equal(setup.dgram.sent, false, 'the dgram packet should not be sent');
     t.end();
@@ -147,6 +173,10 @@ test('log4js gelfAppender', (batch) => {
   batch.test('with a null log message', (t) => {
     const setup = setupLogging();
     setup.logger.info(null);
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
 
     t.ok(setup.dgram.sent);
 
@@ -164,6 +194,10 @@ test('log4js gelfAppender', (batch) => {
       facility: 'nonsense'
     });
     setup.logger.debug('Just testing.');
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
 
     const dgram = setup.dgram;
     t.test('the dgram packet should pick up the options', (assert) => {
@@ -186,6 +220,10 @@ test('log4js gelfAppender', (batch) => {
     const setup = setupLogging();
     setup.exitHandler();
 
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
+
     t.ok(setup.dgram.socket.closed);
     t.end();
   });
@@ -203,6 +241,10 @@ test('log4js gelfAppender', (batch) => {
     setup.compress.shouldError = true;
     setup.logger.info('whatever');
 
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
+
     t.equal(setup.console.message, 'oh noes');
     t.end();
   });
@@ -213,6 +255,10 @@ test('log4js gelfAppender', (batch) => {
         type: 'madeuplayout',
         earlgrey: 'yes, please'
       }
+    });
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
     });
 
     t.test('should pass options to layout', (assert) => {
@@ -231,15 +277,21 @@ test('log4js gelfAppender', (batch) => {
       facility: 'nonsense',
       customFields: {
         _every1: 'Hello every one',
-        _every2: 'Hello every two'
+        _every2: 'Hello every two',
+        notThisOne: 'Move along'
       }
     });
     const myFields = {
       GELF: true,
       _every2: 'Overwritten!',
-      _myField: 'This is my field!'
+      _myField: 'This is my field!',
+      _id: 'This should be skipped'
     };
     setup.logger.debug(myFields, 'Just testing.');
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
 
     const dgram = setup.dgram;
     t.test('the dgram packet should pick up the options', (assert) => {
@@ -256,7 +308,41 @@ test('log4js gelfAppender', (batch) => {
       assert.equal(message._every1, 'Hello every one'); // the default value
       assert.equal(message._every2, 'Overwritten!'); // the overwritten value
       assert.equal(message._myField, 'This is my field!'); // the value for this message only
+      assert.notOk(message.notThisOne); // should not be included
+      assert.notOk(message._id); // should not be included
       assert.equal(message.short_message, 'Just testing.'); // skip the field object
+      assert.end();
+    });
+    t.end();
+  });
+
+  batch.test('with custom fields and no log message', (t) => {
+    const setup = setupLogging();
+    setup.logger.debug({ GELF: true, _pants: 'yep' });
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
+
+    const message = JSON.parse(setup.compress.uncompressed);
+    t.test('should still log the custom fields', (assert) => {
+      assert.equal(message._pants, 'yep');
+      assert.end();
+    });
+    t.end();
+  });
+
+  batch.test('with an empty log message', (t) => {
+    const setup = setupLogging();
+    setup.logger.debug();
+
+    t.tearDown(() => {
+      setup.log4js.shutdown(() => {});
+    });
+
+    const message = JSON.parse(setup.compress.uncompressed);
+    t.test('should still send a message', (assert) => {
+      assert.equal(message.short_message, '');
       assert.end();
     });
     t.end();
